673e9f140697b56d985f8642d5319cf3b8ce5e7a
Documenter.jl <documenter@juliadocs.github.io>
2024-11-27 16:15:26 +0000

    build based on e27eeec

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AJD","category":"page"},{"location":"#AJD","page":"Home","title":"AJD","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AJD.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AJD]","category":"page"}]
+}
