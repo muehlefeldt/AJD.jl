87f3e039e1e1513bd877d5fdbb4b9cbd30efa5cf
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2024-12-09 21:51:58 +0100

    Reorganisation of the tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,7 @@ using Test
 using Diagonalizations
 
 @testset "AJD.jl" begin
-    @testset "pythoncomparison" begin
-        @test 1+2 == 1 + 2
-    end 
     # Write your tests here.
     include("test_python_function.jl")
+    include("test_diagonalizations_against_python.jl")
 end
