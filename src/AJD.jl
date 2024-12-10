3a82dcf5e893d13ab3a33ad46f22af41f07274d0
gericke-n <160725722+gericke-n@users.noreply.github.com>
2024-12-10 22:13:04 +0100

    checkout

--- src/AJD.jl
+++ src/AJD.jl
@@ -1,5 +1,10 @@
 module AJD
 
-# Write your package code here.
-
+function Is_Commuting(A::AbstractMatrix, B::AbstractMatrix)
+    return A*B == B*A #check if the Matrices are commuting else the Diag won't work
+end
+function get_diag_elements(A::AbstractMatrix)
+        
+end
+export Is_Commuting
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using AJD
 using Test
 
-@testset "AJD.jl" begin
-    # Write your tests here.
-end
+@testset "Is_Commuting" begin
+    @test Is_Commuting([1 0; 1 0], [1 0; 1 0]) == true
+    @test Is_Commuting([1 2; 1 2], [2 2; 2 2]) == false
+ end    
