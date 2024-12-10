066c89199f1488361b736da595f91e976d9afc37
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2024-12-10 11:50:16 +0100

    Comment.

--- test/test_python_function.jl
+++ test/test_python_function.jl
@@ -10,7 +10,8 @@ def py_multiply(x, y):
     return x * y
 """
 
+# Example implementation of tests comparing Julia code to Python implementation.
 @testset "pythoncomparison" begin
     @test py"py_add"(1, 2) == 1 + 2
     @test py"py_multiply"(3, 4) == multiply(3, 4)
-end 
+end
