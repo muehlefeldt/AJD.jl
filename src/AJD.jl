51e5c9d448c7b252af3af51de0be29c73811191f
Nicholas Gericke <nicholas.gericke@fsd.tu-berlin.de>
2024-12-10 12:16:34 +0100

    basic methods for testing if the matrices that should be diagonilized meet basic criterias like commutation

--- src/AJD.jl
+++ src/AJD.jl
@@ -1,5 +1,24 @@
 module AJD
 
 # Write your package code here.
-
+function Is_Commuting(A::AbstractMatrix, B::AbstractMatrix)
+    return A*B == B*A
+end
+function Is_Same_size(A::AbstractMatrix, B::AbstractMatrix)
+    return size(A) == size(B)
+end
+function Is_Symmetric(A::AbstractMatrix)
+    return size(A,1) == size(A,2)
+end
+function get_Diag_elements(A::AbstractMatrix)
+    #Code found under: https://discourse.julialang.org/t/off-diagonal-elements-of-matrix/41169/4
+    #best method regarding compilation time found so far
+        row, column = size(ma)
+        [ma[index_row, index_column] for index_row = 1:row, index_column = 1:column if index_row != index_column]
+      end 
+end
+export Is_Commuting
+export Is_Same_size
+export Is_Symmetric
+export get_Diag_elements
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using AJD
 using Test
 
-@testset "AJD.jl" begin
-    # Write your tests here.
+@testset "Is_Commuting" begin
+        A = [1 1; 1 1]
+        C = [0 2; 1 2]
+        @test Is_Commuting(A,A) == true
+        @test Is_Commuting(A,C) == false
+end
+@testset "Is_Same_size" begin
+    A = ones(2,2)
+    B = ones(1,2)
+    @test Is_Same_size(A,A) == true
+    @test Is_Same_size(A,B) == false
+end
+@testset "Is_Symmetric" begin
+    A = ones(2,2)
+    B = ones(2,1)
+    @test Is_Symmetric(A) == true
+    @test Is_Symmetric(B) == false
 end
