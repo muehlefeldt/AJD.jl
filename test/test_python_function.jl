0dac728ab81ff3b833289c6b3b4e5208bfeaaf09
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2024-12-10 11:41:43 +0100

    Test case of module function against Python.

--- test/test_python_function.jl
+++ test/test_python_function.jl
@@ -1,11 +1,16 @@
 using Test
 using PyCall
+using AJD
 
 py"""
 def py_add(x, y):
     return x + y
+
+def py_multiply(x, y):
+    return x * y
 """
 
 @testset "pythoncomparison" begin
     @test py"py_add"(1, 2) == 1 + 2
+    @test py"py_multiply"(3, 4) == multiply(3, 4)
 end 
