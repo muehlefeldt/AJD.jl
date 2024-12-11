97ff3ba0a243efe47ccb38d0040d3dee7a2b7fd2
gericke-n <160725722+gericke-n@users.noreply.github.com>
2024-12-11 18:23:29 +0100

    fixed some bugs and more comments

--- src/AJD.jl
+++ src/AJD.jl
@@ -48,18 +48,20 @@ end
 function JADE(A::AbstractArray;threshold = 10e-18, max_iter = 1000)
     #A concatenate in third dimension by  A =[[1 2; 1 2];;;[2 3; 4 5]]
     #only works for Real Matrices of A but not complex
-    A = Float64.(A)
+    A = Float64.(A) #if the Array isn't already of Float64
+    
     rows, columns, k = size(A)
 
     #initialize the apporximate joint eigenvecotrs as described in Cardoso
-    V = (1.0)*I(rows)+zeros(rows, columns)
+    V = (1.0)*I(rows)+zeros(rows, columns) #needs to be added otherwise we cannot manipulate the non diag. elements of V
+    
     iteration_step = 0
-    #Calculate h first for all the entries in the matrix
 
-    active = true
+    active = true #flag if threshold is reached
     while iteration_step >= max_iter || active == true
    
         active = false
+
         for row = 1:rows
             for column = 2:columns
                 h_diag = A[row,row,:] - A[column,column,:] #first entry of h
@@ -83,12 +85,14 @@ function JADE(A::AbstractArray;threshold = 10e-18, max_iter = 1000)
                     end
                     V[:,pair] = transpose(R*transpose(V[:,pair]))
                 end
-                iteration_step += 1
+                
             end
         end 
-
+        iteration_step += 1
     end
+    
     return  A,V
+
 end
 
 export Is_Commuting
