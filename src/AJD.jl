9a3e09ca2b66df23a3cd44d12146b545efb13407
jemma-blue <jemma.jing23@gmail.com>
2024-12-11 16:23:14 +0100

    version02 can deal with real matrices without errors

--- src/AJD.jl
+++ src/AJD.jl
@@ -1,11 +1,17 @@
 module AJD
 function testAJD(A,jthresh)
+    # Version02, 11.Dec.2024
+    # This version only works for matrix with real valued entries
 	# Input:
 	# A is a mxnm matrix,(A1,...,An),each with dimension nxn
 	# thresh is a threshold for approximation stop, normally = 10e-8
+    # Output:
+    # V : is a  mxm matrix, which accumulates givens rotations G in each iteration
+    # A : is a mxnm matrix, which contains [VA1V',...,VAnV']
+    
 	m,nm = size(A)
 
-	V= Matrix{ComplexF64}(I,m,m)
+	V= Matrix{Float64}(I,m,m)
 
 	# Initializing flag
 	flag = true
@@ -30,13 +36,14 @@ function testAJD(A,jthresh)
 				# Calculating eigenvalue and eigenvector of 
 				F = eigen(real(B*(g*g')B'))
 				sp = sortperm(F.values)
+                # choose the eigenvector with largest eigenvalue
 				angles = F.vectors[:,sp[3]]
 				if angles[1] < 0
 					angles =-angles
 				end
-				# 计算 givens rotation 的参数
+				# calculate the parameters for givens rotation
 				c = sqrt(0.5+angles[1]/2)
-				s = 0.5*(angles[2]-im*angles[3])/c
+				s = 0.5*(angles[2])/c
 				
 				#update matrices A and V by a givens rotation
 				if abs(s)>jthresh
@@ -45,29 +52,24 @@ function testAJD(A,jthresh)
 					pair = [p,q]
 					G = [c -conj(s); s c]
 
-					
+					# update V, which accumulates givens rotations
 					V[:,pair] = V[:,pair]*G
 					
 
 					
-					# Real matrix A
+					# update related two rows, p,q, of Real matrix A by one givens rotation 
 					A[pair,:] = G'*A[pair,:]
 
-					# complex matrix
-					# M[pair,:] = G'*M[pair,:]
 					
 
 
 
-					#####REAL
+
+                    # update related two columns ,p,q of Real matrix A by one givens rotation
 					A[:,Ip] = c*A[:,Ip]+s*A[:,Iq] 
-					#
-					# M[:,Ip] = c*M[:,Ip]+s*M[:,Iq] 
-					#####REAL A
+
 					A[:,Iq] = -conj(s)*A[:,Ip]+c*A[:,Iq]
 
-					#complex matrix M
-					# M[:,Iq] = -conj(s)*M[:,Ip]+c*M[:,Iq]
 					
 				end
 
