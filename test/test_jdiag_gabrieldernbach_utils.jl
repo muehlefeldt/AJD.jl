a46d5a9238ae3aa57bf153987f4502596d1a50f1
Nicholas Gericke <nicholas.gericke@fsd.tu-berlin.de>
2025-01-03 19:32:07 +0100

    - testset fixed

--- test/test_jdiag_gabrieldernbach_utils.jl
+++ test/test_jdiag_gabrieldernbach_utils.jl
@@ -1,5 +1,5 @@
 using PosDefManifold
-using LinearAlgebra: opnorm
+using LinearAlgebra
 using AJD: iscommuting, issamesize, isstrictly_diagonally_dominant, get_offdiag_elements, get_diag_elements,frobenius_offdiag_normation, sort_offdiag_elements
 
 @testset "iscommuting" begin
@@ -40,7 +40,8 @@ end
     @test sort_offdiag_elements(input_1D) == float.([2,4,3,7,6,8])
     input = [[1 2 3; 4 5 6; 7 8 9];;;[0 2 4; 4 2 1; 9 0 1]]
     @test sum(sort_offdiag_elements(input)) == sum(get_offdiag_elements(input))
-    input = reshape(repeat(Matrix(1.0I,2,2)))
+    input = reshape(repeat(Matrix(1.0I,2,2), outer = (1,2)),2,2,2) # has dimension 2x2x2 -> number of offdiag_el = 2*2
+    @test sort_offdiag_elements(input) == zeros(4)
 end
 #TODO: Old code, if not needed should be deleted and the functions as well
 # @testset "inf_off_diag_normation" begin
