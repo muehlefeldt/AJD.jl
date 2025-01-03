76bd6c9f1f40d07bc77fef2d11d36f914a16c10d
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-03 22:39:53 +0100

    Comments.

--- test/test_nondiagonality.jl
+++ test/test_nondiagonality.jl
@@ -1,8 +1,13 @@
+# Testing the Nondiagonality of F' * A * F.
+# F being the calculated filter and A being an input matrix.
+# The result nonDiagonality() indicates the "distance" from a perfect diagonal matrix.
+
 using AJD
 using Diagonalizations
 
 # Define the acceptable error level.
 # How far the diagonalised matrices can be away from a perfect diagonal matrix.
+# Diagonalizations.jl set the error level to 1e-6.
 const accepted_error = 1e-6
 
 @testset "Nondiagonality" begin
@@ -15,7 +20,9 @@ const accepted_error = 1e-6
 end
 
 @testset "Nondiagonality Complex" begin
+    # Select algorithms supporting complex matrices.
     for name in ["jdiag_edourdpineau", "jdiag_gabrieldernbach"]
+        # Generate complex test input and calculate filter.
         test_input = AJD.random_normal_commuting_matrices(10, 6; complex=true)
         result = diagonalize(test_input, algorithm=name)
         
