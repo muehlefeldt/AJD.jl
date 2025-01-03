392b67bb03fed70faa27b76b32c030d9263395ca
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-03 18:55:10 +0100

    create_linear_filter() and complex commuting matrices.

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,9 @@
 using LinearAlgebra
+using Diagonalizations
 
 """
+    random_matrices(n::Int, m::Int)
+
 Generate m random matrices of size n x n
 """
 function random_matrices(n::Int, m::Int)
@@ -26,14 +29,28 @@ function random_commuting_matrices(n::Int, m::Int)
 end
 
 """
+    random_normal_commuting_matrices(n::Int, m::Int; complex::Bool=false)
+    
 Generate m random normal commuting matrices of size n x n
 These can be exactly diagonalized
 
 M_i * M_j = M_j * M_i for all i,j
 M_i*M_i' = M_i'*M_i for all i
 """
-function random_normal_commuting_matrices(n::Int, m::Int)
+function random_normal_commuting_matrices(n::Int, m::Int; complex::Bool=false)
     Q, _ = qr(rand(n,n))
     Q = Matrix(Q)
+    if complex
+        return [Q*Diagonal(rand(ComplexF64, n))*Q' for _ in 1:m]
+    end
     return [Q*Diagonal(rand(n))*Q' for _ in 1:m]
+end
+
+"""
+Create LinearFilter object as introduced by Diagonalizations.jl.
+Output of AJD.jl follows convention of Diagonalizations.jl and produces a LinearFilter.
+"""
+function create_linear_filter(A::Matrix{T} where {T<:Number})
+    args=("Approximate Joint Diagonalization", false)
+    return LinearFilter(A, Matrix(A'), nothing, nothing, nothing, nothing, args...)
 end
