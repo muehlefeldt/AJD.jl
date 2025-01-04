aa16c8a4465b3aeed28c564818995a2e1a27008d
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-04 17:19:21 +0100

    Use and test of is_commuting().
    Also test of is_same_size().

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,14 +1,33 @@
+# Test utility functions.
 using AJD
 
-
+# Test the generation of random, commuting matrices.
+# Used to create test data for the actual diagonalization.
+# Real and complex matrices are tested.
 @testset "Generate commuting matrices" begin
     matrices = AJD.random_normal_commuting_matrices(4, 10)
     for index in 1:length(matrices)-1
-        @test isapprox(matrices[index] * matrices[index+1], matrices[index+1] * matrices[index])
+        @test AJD.is_commuting(matrices[index], matrices[index+1])
     end
 
     matrices = AJD.random_normal_commuting_matrices(4, 10; complex=true)
     for index in 1:length(matrices)-1
-        @test isapprox(matrices[index] * matrices[index+1], matrices[index+1] * matrices[index])
+        @test AJD.is_commuting(matrices[index], matrices[index+1])
     end
+end
+
+# Verify basic function of is_commuting().
+@testset "AJD.is_commuting()" begin
+    A = [1 1; 1 1]
+    C = [0 2; 1 2]
+    @test AJD.is_commuting(A,A)
+    @test !AJD.is_commuting(A,C)
+end
+
+# Verify basic function of is_samesize().
+@testset "AJD.is_same_size()" begin
+    A = ones(2,2)
+    B = ones(1,2)
+    @test AJD.is_same_size(A,A) == true
+    @test AJD.is_same_size(A,B) == false
 end
