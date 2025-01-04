fd6536d02a1042e485a85365301bce77522a0732
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-04 19:00:48 +0100

    Test nondiagonality of real single matrix.

--- test/test_nondiagonality.jl
+++ test/test_nondiagonality.jl
@@ -19,6 +19,14 @@ const accepted_error = 1e-6
     end
 end
 
+@testset "Nondiagonality Single Matrix" begin
+    for name in ["jdiag_gabrieldernbach", "jdiag_cardoso", "jdiag_edourdpineau"]
+        test_input = AJD.random_normal_commuting_matrices(10, 1)
+        result = diagonalize(test_input, algorithm=name)
+        
+        @test mean([nonDiagonality(result.iF * A * result.F) for A in test_input]) < accepted_error
+    end
+
 @testset "Nondiagonality Complex" begin
     # Select algorithms supporting complex matrices.
     for name in ["jdiag_edourdpineau", "jdiag_gabrieldernbach"]
