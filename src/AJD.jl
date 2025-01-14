2da697cc4fc4ea00007c63d95d2208871d993711
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-14 22:45:59 +0100

    Plotting.jl introduced. Code restructure.
    Simplifies the structure of diagonalize().

--- src/AJD.jl
+++ src/AJD.jl
@@ -1,15 +1,16 @@
 module AJD 
 using LinearAlgebra
 using BenchmarkTools
-using Plots
 
 # Import different JDiag algorithms.
 include("jdiag_algorithms/jdiag_cardoso.jl")
 include("jdiag_algorithms/jdiag_gabrieldernbach.jl")
 include("jdiag_algorithms/jdiag_edourdpineau.jl")
 include("jdiag_algorithms/FFDiag.jl")
+
 # Utility function import.
 include("utils.jl")
+include("plotting.jl")
 
 """
     diagonalize(
@@ -44,37 +45,39 @@ function diagonalize(
 
     if algorithm in ["jdiag", "jdiag_gabrieldernbach"]
         F, B, error_array = jdiag_gabrieldernbach!(A, max_iter = max_iter, threshold = threshold, plot_convergence = plot_convergence)
-        if plot_matrix
-            plot_matrix_heatmap(F, B)
-        end
-        if plot_convergence
-            plot_convergence_lineplot(error_array, algorithm)
-        end
-        return create_linear_filter(F)
-    end
 
-    if algorithm == "jdiag_cardoso"
+    elseif algorithm == "jdiag_edourdpineau"
+        F, B, error_array = jdiag_edourdpineau(A, iter = max_iter)
+
+    elseif algorithm == "jdiag_cardoso"
         if typeof(A) <: AbstractArray{<:AbstractArray{<:Real}} 
-            _,F ,_ = jdiag_cardoso(hcat(A...), threshold)
-            return create_linear_filter(F)
+            F, B, error_array = jdiag_cardoso(hcat(A...), threshold)
         else
             throw(ArgumentError("Not supported for set of Matrices containing imaginary values!"))
         end
+
+    elseif algorithm in ["FFD", "ffd", "ffdiag"]
+        F, B, error_array = FFD!(A)
+        @info F
+        F = Matrix(F') # F' Due to some FFDiag properties.
+
+    else
+        # If no vaild algorithm selected throw an error.
+        throw(ArgumentError("No valid algorithm selected from available"))
     end
 
-    if algorithm == "jdiag_edourdpineau"
-        F, _, _ = jdiag_edourdpineau(A)
-        #plotting(F)
-        return create_linear_filter(F)
+    # Plotting output if so selected by the user.
+    if plot_matrix
+        # Illustrate Filter and diagonlised matrices.
+        plot_matrix_heatmap(F, B)
     end
 
-    if algorithm in ["FFD", "ffd", "ffdiag"]
-        _,F = FFD!(copy(A))
-        return create_linear_filter(Matrix(F'))
+    if plot_convergence
+        # Show convergence of the error.
+        plot_convergence_lineplot(error_array, algorithm)
     end
 
-    # If no vaild algorithm selected throw an error.
-    throw(ArgumentError("No valid algorithm selected from available"))
+    return create_linear_filter(F)
 end
 
 """
@@ -113,35 +116,6 @@ function ajd_benchmark(n_dims::Int, n_matrices::Int)
 
     # Return BenchmarkGroup for further evaluation.
     return results
-end
-
-"""
-    plot_matrix_heatmap(filter::AbstractMatrix, diag_matrices)
-
-Plot a heatmap of the calculated filter matrix and the mean of the diagonlized matrices.
-"""
-function plot_matrix_heatmap(filter::AbstractMatrix, diag_matrices) 
-    theme(:dark)
-    # Subplot of the filter matrix.
-    filter_plot = heatmap(filter, yflip=true, title="Filter Matrix")
-
-    # Subplot of the mean of all the diagonalised matrices.
-    mean_diag_plot = heatmap(mean(diag_matrices, dims=3)[:, :, 1], yflip=true, title="Mean Diagonalized Matrices")
-
-    # Combine subplots and 
-    combined_plot = plot(filter_plot, mean_diag_plot, layout=(2, 1), legend=false, size=(400, 800))
-    display(combined_plot)
-end
-
-"""
-    plot_convergence_lineplot(error_array::AbstractArray, name::String)
-
-Plot the convergence error as recorded during the algorithm execution.
-"""
-function plot_convergence_lineplot(error_array::AbstractArray, name::String)
-    theme(:dark)
-    line_plot = plot(error_array, w=3, title="Error Convergence", label=name)
-    display(line_plot)
 end
 
 export diagonalize, ajd_benchmark
