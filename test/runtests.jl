e2d9073b2c48d92da65349bbdef21712274fbe7d
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-15 15:17:21 +0100

    White space.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using AJD
 using Test
 using Diagonalizations
 using LinearAlgebra
+
 @testset "AJD.jl" begin
     include("test_jdiag_gabrieldernbach.jl")
     include("test_jdiag_gabrieldernbach_utils.jl")
