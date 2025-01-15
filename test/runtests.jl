ebd7f77718725484d38089334404bfdfbe9346e5
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-15 18:04:59 +0100

    Temp. removal of tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,14 +4,14 @@ using Diagonalizations
 using LinearAlgebra
 
 @testset "AJD.jl" begin
-    include("test_jdiag_gabrieldernbach.jl")
-    include("test_jdiag_gabrieldernbach_utils.jl")
-    include("test_jdiag_cardoso.jl")
-    include("test_jdiag_eduardpineau.jl")
-    include("test_linearfilter.jl")
-    include("test_nondiagonality.jl")
-    include("test_utils.jl")
-    include("test_benchmark.jl")
-    include("test_ffdiag.jl")
+    #include("test_jdiag_gabrieldernbach.jl")
+    #include("test_jdiag_gabrieldernbach_utils.jl")
+    #include("test_jdiag_cardoso.jl")
+    #include("test_jdiag_eduardpineau.jl")
+    #include("test_linearfilter.jl")
+    #include("test_nondiagonality.jl")
+    #include("test_utils.jl")
+    #include("test_benchmark.jl")
+    #include("test_ffdiag.jl")
     include("test_plotting.jl")
 end
