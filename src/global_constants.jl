1c959ff4aca9e3aa2a40169f178ce819fbddc5af
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-16 17:26:16 +0100

    COMLPLEX_ALGORITHMS.

--- src/global_constants.jl
+++ src/global_constants.jl
@@ -1,7 +1,13 @@
-# List of all algorithms keywords. Used in tests.
+"List of all algorithms keywords. Used in tests."
 const ALL_ALGORITHMS = [
     "jdiag_gabrieldernbach",
     "jdiag_edourdpineau",
     "jdiag_cardoso",
     "ffdiag"
     ]
+
+"List of all algorithms supporting complex matrices."
+const COMLPLEX_ALGORITHMS = [
+    "jdiag_gabrieldernbach",
+    "jdiag_edourdpineau"
+]
