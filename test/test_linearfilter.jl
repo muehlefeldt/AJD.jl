d9a48ebe22aa13bf42954b4e7030e8ba9412a288
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-16 15:40:58 +0100

    Use global constant to check all algorithms.

--- test/test_linearfilter.jl
+++ test/test_linearfilter.jl
@@ -2,12 +2,12 @@
 
 @testset "LinearFilter" begin
     # Iterate over all implemented algorithms.
-    for name in ["jdiag_gabrieldernbach", "jdiag_cardoso", "jdiag_edourdpineau"]
+    for name in AJD.ALL_ALGORITHMS
         test_input = AJD.random_normal_commuting_matrices(10, 6)
         result = diagonalize(test_input, algorithm=name)
         
         # Check for LinearFilter.
-        @test typeof(result) == LinearFilter
+        @test typeof(result) <: LinearFilter
 
         # Check Linearfilter contents.
         @test typeof(result.F) <: AbstractMatrix{T} where {T<:Number}
