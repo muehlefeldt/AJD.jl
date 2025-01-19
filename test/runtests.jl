81aee73e9b8b9bca90e780066615e341b86f6275
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-19 21:42:27 +0100

    New test sets.

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,11 @@ using LinearAlgebra
     include("test_jdiag_cardoso.jl")
     include("test_jdiag_eduardpineau.jl")
     include("test_linearfilter.jl")
+
+    # Test the actual result of the algorithm by cheking how nondiagonal the matrices are.
     include("test_nondiagonality.jl")
+    include("test_nondiagonality_time_correlated.jl")
+
     include("test_utils.jl")
     include("test_benchmark.jl")
     include("test_ffdiag.jl")
