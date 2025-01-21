1097508324a04a8aa9081f4d225dfc5f0bfb4d28
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-21 17:06:31 +0100

    Plot larger and theme change.
    Theme changed for better visibility of small values.

--- src/plotting.jl
+++ src/plotting.jl
@@ -18,11 +18,11 @@ function get_plot(
         name::String)
 
     # Select PLots.jl theme.
-    theme(:dark)
+    theme(:vibrant)
 
     # Get the plots and returned one combined plot.
     filter_plot = heatmap(real.(filter), yflip=true, title="Filter Matrix")
     mean_diag_plot = heatmap(real.(mean((diag_matrices), dims=3)[:, :, 1]), yflip=true, title="Mean Diagonalized Matrices")
     error_plot = plot(error_array, w=3, title="Error Convergence", label=name)
-    return plot(filter_plot, mean_diag_plot, error_plot, layout=(3, 1), legend=false, size=(400, 1200))
+    return plot(filter_plot, mean_diag_plot, error_plot, layout=(3, 1), legend=false, size=(800, 1200))
 end
