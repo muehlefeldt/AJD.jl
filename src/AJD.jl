b22c7ce4a6b0a5fbb2010b9bf892847f2610c9eb
Nicholas Gericke <nicholas.gericke@fsd.tu-berlin.de>
2025-01-27 13:15:04 +0100

    - AJD.jl fixed spelling in throw Argument error of plot ony_plot -> only_plot -FFDiag now has docstrings and some explanation - changed ffdiag error_array to array of float64 for type stability - changed the handling of NaN values to show a warning and ending the loop instead of the isfinite for W[i,j] and setting the entry to 0.0 for clarity - changed test_non_diagonality to account for this change with a isfinite test for single_matrix testcase

--- src/AJD.jl
+++ src/AJD.jl
@@ -73,7 +73,7 @@ function diagonalize(
         )
         p = get_plot(F, B, error_array, algorithm)
     else
-        throw(ArgumentError("Please use symbol ony_plot=:plot to generate plots."))
+        throw(ArgumentError("Please use symbol only_plot=:plot to generate plots."))
     end
     return p
 end
