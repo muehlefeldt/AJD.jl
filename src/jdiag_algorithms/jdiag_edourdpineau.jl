2a4f1257ffa200809b145df997bdd3acd6645fca
Nicholas Gericke <160725722+gericke-n@users.noreply.github.com>
2025-01-30 21:25:14 +0100

    Issue #131 (#144)
    * - added type output to cat in jdiags except for cardoso
    - added type conversion of eduardpineau from int to float
    
    * -rm int conversion in jdiag_eduarpineau for now since testing is hard and type can't be converted as easily
    - will add int conversion to diagonalize in future update
    -rm a progressbar update in file
    
    * - added relative error to FFDiag, and gabrieldernbach, tried cardoso but didn't want to change the update condition too much, which lead to aborting it
    - documentation of all algorithms in source code
    - TODO: Change the docstrings of algorithms to be more in line with utils.
    
    * - fixed bug (thresh changed to threshold, was missing before)
    
    * - missed a jthresh in cardoso. should be fixed now...
    
    ---------
    
    Co-authored-by: Nicholas Gericke <nicholas.gericke@fsd.tu-berlin.de>

--- src/jdiag_algorithms/jdiag_edourdpineau.jl
+++ src/jdiag_algorithms/jdiag_edourdpineau.jl
@@ -44,7 +44,7 @@ end
 """
     jdiag_edourdpineau(X::Vector{M}; iter=100, eps=1e-3)
         where {T<:Union{Real,Complex},M<:AbstractMatrix{T}}
-
+*X
 Diagonalize a set of matrices using the Jacobi method ("Jacobi Angles for Simultaneous Diagonalization").
 Code adapted from [Edouardpineaus Python implementation](https://github.com/edouardpineau/Time-Series-ICA-with-SOBI-Jacobi)
 """
@@ -55,7 +55,8 @@ function jdiag_edourdpineau(
     atol = eps(),
 ) where {T<:Number,M<:AbstractMatrix{T}}
 
-    
+    #convert to 3 dimensional matrix and concatenate in the third dimension
+    #will also reset dimensions of matrix if OffsetArray
     Xm = cat(X..., dims = 3)::AbstractArray{<:Number}
     m = length(X)
     n = size(X[1], 1)
@@ -99,7 +100,7 @@ function jdiag_edourdpineau(
 
         # Update progress info.
         update!(progress_bar, diff)
-
+        #check if absolute or relative threshold is reached
         if diff < atol || diff < rtol * norm
             break
         end
