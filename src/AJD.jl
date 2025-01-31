62a0faf84b674c002343a621c9327ca698812536
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2025-01-31 21:36:54 +0100

    Docstring.

--- src/AJD.jl
+++ src/AJD.jl
@@ -27,11 +27,28 @@ include("utils_test_data.jl")
 include("global_constants.jl")
 
 """
+(1)
     diagonalize(
         M::Vector{<:AbstractMatrix{<:Number}};
         algorithm::AbstractDiagonalization = JDiagEdourdPineau(),
         max_iter::Int = 1000,
         threshold::AbstractFloat = eps(),
+    )
+
+(2)
+    diagonalize(
+        M::Vector{<:AbstractMatrix{<:Number}},
+        only_plot::Symbol;
+        algorithm::AbstractDiagonalization = JDiagGabrielDernbach(),
+        max_iter::Int = 1000,
+        threshold::AbstractFloat = eps(),
+    )
+
+(3)
+    diagonalize(
+        benchmark::Symbol,
+        n_dims::Int,
+        n_matrices::Int,
     )
 
 Calculate joint diagonalization of multiple input matrices ``M_k``.
@@ -43,11 +60,28 @@ The matrices can be of types Float64 or Complex.
 
 See the [Getting Started Guide](https://muehlefeldt.github.io/AJD.jl/dev/getting-started/) for information on the algorithms.
 
-Inputs:
+# Dispatch (1)
+## Inputs
 * `M`: Vector of matrices (requiered).
-* `algorithm = ...`: Selected algorithm from `JDiagGabrielDernbach()`, `JDiagEdourdPineau()`, `JDiagCardoso()` or `FFDiag()`.
-* `max_iter = ...`: Maximum iteration step as integer.
-* `threshold = ...`: Desired threshold minimizing the off-diagonal elements.
+* `algorithm`: Selected algorithm from `JDiagGabrielDernbach()`, `JDiagEdourdPineau()`, `JDiagCardoso()` or `FFDiag()`.
+* `max_iter`: Maximum iteration step as integer.
+* `threshold`: Desired threshold minimizing the off-diagonal elements.
+## Output
+* Return LinearFilter object. Filter `fil` contains filter matrix `fil.F` and the inverse `fil.iF.`
+
+
+# Dispatch (2)
+## Inputs
+* Additional symbol used to generate overview plot of the result. `Use diagonalize(M, :plot)`.
+## Output
+* Overview plot. Shows heatmap of the filter matrix, heatmap of the mean of the diagonalized matrices and the vonvergence behaviour of the algorithm.
+
+# Dispatch (3)
+## Inputs
+* Symbol `:benchmark` used as `diagonalize(:benchmark, 10, 10)`.
+* Automatic benchmark is run comparing JDiag and FFDiag algorithms. Using `n_dims` ``\\times`` `n_dims` matrices of count `n_matrices.` 
+## Output
+* BenchmarkGroup of package `BenchmarkTools` comparing the algorithms using diffrent types of test data.
 """
 function diagonalize(
     M::Vector{<:AbstractMatrix{<:Number}};
